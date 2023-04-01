08e766bf3b4fd5f525af69f7810b24beccffd1c4
Rijan Neupane <neupanerijan6@gmail.com>
2023-04-01 10:29:55 +0545

    Team Page Added

---
+++ js/teamApi.js
@@ -0,0 +1,52 @@
+
+const teamArea = document.querySelector('.team-members');
+
+url = "../js/teams.json";
+fetch(url)
+    .then((response) => {
+        return response.json();
+    }).then((data) => {
+        for (i = 0; i < data.length; i++) {
+            let teamCard = document.createElement("div");
+            teamCard.classList.add('team-card');
+            teamCard.innerHTML = `
+                <img src="${data[i].img}" alt="${data[i].name}" class="profile-img">
+                <h2 class="team-name">${data[i].name}</h2>
+                <p class="team-depart">${data[i].depart}</p>
+                `;
+            teamArea.appendChild(teamCard);
+        }
+    })
+
+
+// Search Query
+// function filterProduct(value) {
+//     //Button class code
+//     let buttons = document.querySelectorAll(".button-value");
+//     buttons.forEach((button) => {
+//         //check if value equals innerText
+//         if (value.toUpperCase() == button.innerText.toUpperCase()) {
+//             button.classList.add("active");
+//         } else {
+//             button.classList.remove("active");
+//         }
+//     });
+//     //select all cards
+//     let elements = document.querySelectorAll(".card");
+//     //loop through all cards
+//     elements.forEach((element) => {
+//         //display all cards on 'all' button click
+//         if (value == "all") {
+//             element.classList.remove("hide");
+//         } else {
+//             //Check if element contains category class
+//             if (element.classList.contains(value)) {
+//                 //display element based on category
+//                 element.classList.remove("hide");
+//             } else {
+//                 //hide other elements
+//                 element.classList.add("hide");
+//             }
+//         }
+//     });
+// }
