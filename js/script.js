cb8f44b50673446b7121aace09c41feb2d1de42a
Rijan Neupane <neupanerijan6@gmail.com>
2023-04-01 18:38:40 +0545

    Team Member Filter Added

--- js/script.js
+++ js/script.js
@@ -1,26 +1,13 @@
-// JQuery Slick Slider
-jQuery(document).ready(function ($) {
-    $('.teams').slick({
-        speed: 4000,
-        autoplay: true,
-        autoplaySpeed: 0,
-        centerMode: true,
-        cssEase: 'linear',
-        slidesToShow: 1,
-        slidesToScroll: 1,
-        variableWidth: true,
-        infinite: true,
-        initialSlide: 1,
-        arrows: false,
-        buttons: false,
-        pauseOnFocus: true,
-        pauseOnHover: true,
-        mobileFirst: true
-    });
-});
-
 $(document).ready(function(){
     setTimeout(function() {
         $('.site-preloader').fadeOut();
     }, 1900);
 });
+
+// hiding options of team filter
+const options = document.querySelector('.buttons');
+const filterIcon = document.querySelector('.fa-filter');
+
+filterIcon.addEventListener('click', () => {
+    options.classList.toggle("buttons-hide");
+})
--- js/teamApi.js
+++ js/teamApi.js
@@ -8,7 +8,7 @@ fetch(url)
     }).then((data) => {
         for (i = 0; i < data.length; i++) {
             let teamCard = document.createElement("div");
-            teamCard.classList.add('team-card');
+            teamCard.classList.add('team-card', data[i].category);
             teamCard.innerHTML = `
                 <img src="${data[i].img}" alt="${data[i].name}" class="profile-img">
                 <h2 class="team-name">${data[i].name}</h2>
@@ -19,34 +19,24 @@ fetch(url)
     })
 
 
-// Search Query
-// function filterProduct(value) {
-//     //Button class code
-//     let buttons = document.querySelectorAll(".button-value");
-//     buttons.forEach((button) => {
-//         //check if value equals innerText
-//         if (value.toUpperCase() == button.innerText.toUpperCase()) {
-//             button.classList.add("active");
-//         } else {
-//             button.classList.remove("active");
-//         }
-//     });
-//     //select all cards
-//     let elements = document.querySelectorAll(".card");
-//     //loop through all cards
-//     elements.forEach((element) => {
-//         //display all cards on 'all' button click
-//         if (value == "all") {
-//             element.classList.remove("hide");
-//         } else {
-//             //Check if element contains category class
-//             if (element.classList.contains(value)) {
-//                 //display element based on category
-//                 element.classList.remove("hide");
-//             } else {
-//                 //hide other elements
-//                 element.classList.add("hide");
-//             }
-//         }
-//     });
-// }
+// Filter the Team Members
+function filterProduct(value) {
+    //select all cards
+    let elements = document.querySelectorAll(".team-card");
+    //loop through all cards
+    elements.forEach((element) => {
+        //display all cards on 'all' button click
+        if (value == "all") {
+            element.classList.remove("hide");
+        } else {
+            //Check if element contains category class
+            if (element.classList.contains(value)) {
+                //display element based on category
+                element.classList.remove("hide");
+            } else {
+                //hide other elements
+                element.classList.add("hide");
+            }
+        }
+    });
+}
