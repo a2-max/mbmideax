3d43af834c80d24d4a915e87c9b6796214ac9548
Rijan Neupane <neupanerijan6@gmail.com>
2023-06-02 17:51:41 +0545

    Notice PopUp Added

--- js/script.js
+++ js/script.js
@@ -2,4 +2,7 @@ $(document).ready(function () {
     setTimeout(function () {
         $('.site-preloader').fadeOut();
     }, 1900);
+    $('#noticePopUp').modal('show');
+    $('#notice2PopUp').modal('show');
 });
+
